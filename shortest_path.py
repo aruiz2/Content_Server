7824ee5aabc6546d27da687a0ca84b951e1fc250
Alejandro Ruiz <aruiz2@ece028.ece.local.cmu.edu>
2022-03-10 16:30:16 -0500

    implemented djikstra

--- shortest_path.py
+++ shortest_path.py
@@ -15,27 +15,25 @@ def calculate_shortest_paths(graph_map, source):
             shortest_paths[destination] = djikstra(graph_map, nodes_data, source, destination)
     return shortest_paths
 
+
 def djikstra(graph_map, nodes_data, source, destination):
     visited = set(); curr_node = source; nodes = len(graph_map.keys())
-    nodes_data[source] = 0
+    nodes_data[source] = 0; 
+    heap = [(0, source)]; visited = set()
+    for node in graph_map.keys(): 
+        heapq.heappush(heap, (nodes_data[node], node))
+    
+    while heap:
+        curr_dist, curr_node = heapq.heappop(heap)
 
-    for iteration in range(nodes):
-        #Only iterate through not visited nodes
         if curr_node not in visited:
             visited.add(curr_node)
-            heap = []
 
-            for nbor in graph_map[curr_node]:
-                if nbor not in visited:
-                    #Calculate cost to nbor
-                    length = nodes_data[curr_node] + graph_map[curr_node][nbor]
-                    
-                    #Update min length of nbor
-                    if length < nodes_data[nbor]:
-                        nodes_data[nbor] = length
+            for nbor in graph_map[curr_node].keys():
+                nbor_dist = curr_dist + graph_map[curr_node][nbor]
+                
+                if nbor_dist < nodes_data[nbor]:
+                    nodes_data[nbor] = nbor_dist
+                    heapq.heappush(heap, (nbor_dist, nbor))
 
-                    heapq.heappush(heap, [nodes_data[nbor], nbor])
-            
-        if heap: curr_node = heap[0][1]
-    
     return nodes_data[destination]
